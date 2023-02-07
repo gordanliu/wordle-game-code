bc892c05ca991210941ea83d164dbb6a7aeb520d
gordanliu <gordonliu2004@gmail.com>
2023-02-07 17:20:12 -0500

    update 4
    updated colors on keyboard, input only accepts letters

--- Wordle.java
+++ Wordle.java
@@ -8,10 +8,11 @@ public class Wordle {
     ArrayList<String> usedLetters;
 
     final String GREEN_BG = "\u001b[42m";
-    final String YELLOW_BG = "\u001b[43m";
-    final String RESET = "\u001b[0m";
-    final String CYAN_TEXT = "\u001B[36m";
     final String GREEN_TEXT = "\u001B[32m";
+    final String YELLOW_BG = "\u001b[43m";
+    final String YELLOW_TEXT = "\u001B[33m";
+    final String RESET = "\u001b[0m";
+    final String UNDERLINE = "\u001b[4m";
 
     public Wordle(String s) {
         answer = s.toUpperCase();
@@ -21,12 +22,12 @@ public class Wordle {
     }
 
     public void makeGuess(String guess) {
-        guess = guess.toUpperCase();
-        if (guess.length() != 5)
+        if (guess.length() != 5 || !isAlpha(guess))
             System.out.println("Invalid guess!");
-        else if (checkIfUsed(guess))
+        else if (checkIfUsed(guess.toUpperCase()))
             System.out.println("You already used that word!");
         else {
+            guess = guess.toUpperCase();
             guessedWords[attempt] = guess;
             attempt++;
             for (String a : guess.split("")) {
@@ -102,18 +103,35 @@ public class Wordle {
             // iterates through every row
             String[] rowArray = this.keyboard[i].split("");
             for (int j = 0; j < rowArray.length; j++) {
+                String character = rowArray[j].toUpperCase();
 
-                // if key is in usedLetters
-                if (usedLetters.contains(rowArray[j].toUpperCase()))
-                    System.out.print(CYAN_TEXT + rowArray[j] + RESET);
+                // character used
+                if (this.usedLetters.contains(character)) {
+                    boolean yellow = false;
+                    boolean green = false;
 
-                // if key is not in usedLetters
-                else if (!rowArray[j].equals(" "))
-                    System.out.print(rowArray[j]);
+                    // checks through guesses to see if character should be yellow, green, or
+                    // neither
+                    for (String word : this.guessedWords) {
+                        if (word != null) {
+                            if (word.contains(character)) {
+                                if (word.indexOf(character) == answer.indexOf(character))
+                                    green = true;
+                                else if (answer.contains(character))
+                                    yellow = true;
+                            }
+                        }
+                    }
+                    if (green)
+                        System.out.print(UNDERLINE + GREEN_TEXT + character + RESET);
+                    else if (yellow)
+                        System.out.print(UNDERLINE + YELLOW_TEXT + character + RESET);
+                    else
+                        System.out.print(UNDERLINE + character + RESET);
 
-                // spacebars
-                else
-                    System.out.print(" ");
+                    // character not used
+                } else
+                    System.out.print(character);
             }
             System.out.println();
         }
@@ -125,6 +143,10 @@ public class Wordle {
             if (guess.equals(word))
                 return true;
         return false;
+    }
+
+    public boolean isAlpha(String guess) {
+        return guess.matches("[a-zA-Z]+");
     }
 
     public void win() {
